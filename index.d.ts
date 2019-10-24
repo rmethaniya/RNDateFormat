a47579b6823b84fbbb70b97e6b5232c1dc78115e
rmethaniya <ravi.patel9637@gmail.com>
2019-10-24 14:12:11 +0530

    update package.json

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+// should be imported this way:
+// import RNDateFormat from 'react-native-date-format';
+
+declare const _default: {
+    formateMillis: () => Promise<string>;
+};
+
+export default _default;
--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 
-import { NativeModules } from 'react-native';
+// import { NativeModules } from 'react-native';
 
-const { RNDateFormat } = NativeModules;
+// const { RNDateFormat } = NativeModules;
 
-export default RNDateFormat;
+// export default RNDateFormat;
+
+/**
+ * @providesModule react-native-date-format
+ */
+import { Platform, NativeModules } from 'react-native';
+
+var RNDateFormat = NativeModules.RNDateFormat;
+
+if (!RNDateFormat) {
+  // Produce an error if we don't have the native module
+  if (
+    Platform.OS === 'android' ||
+    Platform.OS === 'ios'
+  ) {
+    throw new Error("RNDateFormat undefined");
+  }
+
+  RNDateFormat = require('./default');
+}
+
+export default {
+    formateMillis: function() {
+    return RNDateFormat.formateMillis('1','1');
+  },
+};
